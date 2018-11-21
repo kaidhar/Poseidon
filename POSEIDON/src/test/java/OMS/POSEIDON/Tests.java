8a0315b66613c729fef48f3354715f490f039f9b
Kartikay Dhar <kaidhar@gmail.com>
2018-11-21 17:38:56 +0530

    Merged SAKS and WS.

--- POSEIDON/src/test/java/OMS/POSEIDON/Tests.java
+++ POSEIDON/src/test/java/OMS/POSEIDON/Tests.java
@@ -6,36 +6,30 @@ import org.jdom2.JDOMException;
 import org.json.JSONException;
 import org.testng.annotations.Test;
 
+import com.BaseClass.MultiqunatityAllTenderTypes;
+import com.BaseClass.SingleLineAllTenderTypes;
+
 import oms.Poseidon.Webservices.OMSCalls;
 
 public class Tests {
 
 	@Test
-	public void LTWEBCreateOrder() throws JSONException, IOException, JDOMException, InterruptedException {
+	public void TestsForSAKS() throws JSONException, IOException, JDOMException, InterruptedException {
+
+		SingleLineAllTenderTypes SQATT = new SingleLineAllTenderTypes();
+		//String OrderID = SQATT.SingleLineAmex();
 		
-		String OrderID="100147885";
-		String Banner = "LT";
+		String OrderID="073069669";
+		String Banner = "SAKS";
 		OMSCalls Calls = new OMSCalls();
-		int ResponseCode=Calls.getOrderDetails(OrderID, Banner);
+		int ResponseCode = Calls.getOrderDetails(OrderID, Banner);
 		System.out.println(ResponseCode);
 		Calls.resolveBuyersRemorse(OrderID, Banner);
 		Thread.sleep(5000);
 		Calls.runScheduleAgent(OrderID, Banner);
 		Thread.sleep(5000);
 		Calls.runReleaseAgent(OrderID, Banner);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
-	
+
 }
